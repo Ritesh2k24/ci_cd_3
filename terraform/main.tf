59886a8b773f8876e46cb47e7f6e5f7868c9e52a
RiteshG24AI1050 <g24ai1050@iitj.ac.in>
2026-05-29 05:22:55 +0000

    prod_3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,40 @@
-# 1. Create a VPC
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = "python-app-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
+# Fetch existing IAM Role for EKS Cluster Control Plane
+data "aws_iam_role" "cluster" {
+  name = "AmazonEKSAutoClusterRole"
 }
 
-# 2. Create the EKS Cluster
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+# EKS Cluster Control Plane
+resource "aws_eks_cluster" "main" {
+  name     = "my-eks-cluster"
+  role_arn = data.aws_iam_role.cluster.arn
+  version  = "1.35"
 
-  cluster_name    = "python-app-cluster"
-  cluster_version = "1.35"
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
-
-  # Managed Node Group
-  eks_managed_node_groups = {
-    general = {
-      desired_size = 2
-      min_size     = 1
-      max_size     = 4
-
-      instance_types = ["t3.medium"]
-    }
+  vpc_config {
+    subnet_ids = concat(aws_subnet.public.*.id, aws_subnet.private.*.id)
   }
+}
+
+# Fetch existing IAM Role for Worker Nodes
+data "aws_iam_role" "node_group" {
+  name = "AmazonEKSAutoNodeRole"
+}
+
+# Managed Node Group
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "standard-workers"
+  node_role_arn   = data.aws_iam_role.node_group.arn
+  subnet_ids      = aws_subnet.private.*.id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  instance_types = ["t3.medium"]
 }
